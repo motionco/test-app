7811a5ee00675f23f90c756552550cb8d7ea5041
motionco <motioncomc@gmail.com>
2023-11-27 16:51:53 +0900

    첫파일

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          배포 테스트&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
